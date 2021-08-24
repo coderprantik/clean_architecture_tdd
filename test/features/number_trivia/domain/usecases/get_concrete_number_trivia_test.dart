a477e3a02362dda90b31ca1031ad73a6033476ca
Prantik <coderprantik@gmai.com>
2021-08-24 09:20:54 +0600

    GetConcreteNumberTrivia done

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -29,7 +29,7 @@ void main() {
       when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
           .thenAnswer((_) async => Right(tNumberTrivia));
       // act
-      final result = await usecase.execute(number: tNumber);
+      final result = await usecase(Params(number: tNumber));
       // assert
       expect(result, Right(tNumberTrivia));
       verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
